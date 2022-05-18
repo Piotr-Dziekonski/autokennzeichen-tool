fdc09be2976af4f0be5f1b8c98f9eec37931ee0e
piotr.dziekonski@telekom.de <19938986+Piotr-Dziekonski@users.noreply.github.com>
2022-05-18 10:06:34 +0200

    Add distance between links and open in new tab

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,3 +1,5 @@
+import './Row.css';
+
 type Props = {
   content: {
     Ortskuerzel: string,
@@ -16,8 +18,8 @@ export default function Row(props: Props) {
       <td>{content.Landkreis}</td>
       <td>{content.Bundesland}</td>
       <td>
-        <a href={'https://www.google.com/maps/dir/?api=1&origin=' + encodeURI(content.Ursprung)}>Maps</a>
-        <a href={'https://en.wikipedia.org/w/index.php?title=' + encodeURI(content.Ursprung.toLowerCase())}>Wiki</a>
+        <a href={'https://www.google.com/maps/dir/?api=1&origin=' + encodeURI(content.Ursprung)} target="_blank" rel="noopener noreferrer" className="mapsLink">Maps</a>
+        <a href={'https://en.wikipedia.org/w/index.php?title=' + encodeURI(content.Ursprung.toLowerCase())} target="_blank" rel="noopener noreferrer">Wiki</a>
       </td>
     </tr>
   )
